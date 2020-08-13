d189dc422a75208c4d1ef28b811c4a6ad302155d
Adam Gołąbek <47831052+webemperor@users.noreply.github.com>
2020-08-13 23:06:29 +0200

    create index.html and style.scss

---
+++ main.js
@@ -0,0 +1,41 @@
+let $todoInput; // miejsce, gdzie użytkownik wpisuje treść
+let $alertInfo; // info o braku zadań / konieczności dodania tekstu
+let $addBtn; // przycisk ADD - dodaje nowe elementy do listy
+let $ulList; // nasza lista zadań, tagi <ul></ul>
+let $newTask; // nowo dodany LI, nowe zadanie
+let $allTasks; // lista wszystkich dodanych LI
+let $idNumber = 0; // ID dodawane do każdego nowego zadania
+let $popup; //pobrany popup
+let $popupInfo; // alert w popupie, jak się doda pusty tekst
+let $editedTodo; // edytowany Todo
+let $popupInput; //tekst wpisywany w inputa w popup'ie
+let $addPopupBtn; // przycisk "zatwierdź" w popup'ie
+let $closeTodoBtn //przycisk od zamykania popup'a
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+}
+
+const prepareDOMElements = () => {
+    $todoInput = document.querySelector('.todo-input');
+    $alertInfo = document.querySelector('.alert-info');
+    $addBtn = document.querySelector('.add-btn');
+    $ulList = document.querySelector('.todo-list ul');
+    $allTasks = document.getElementsByTagName('li');
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popup-info');
+    $popupInput = document.querySelector('.popup-input');
+    $addPopupBtn = document.querySelector('.accept');
+    $closeTodoBtn = document.querySelector('.cancel');
+}
+
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask);
+}
+
+const addNewTask = () => {
+    console.log('ok');
+}
+
+document.addEventListener('DOMContentLoaded', main);
